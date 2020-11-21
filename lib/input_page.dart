0b419d53b21a6e63dc8a562bf4782e7b91be2d84
samsung <ssossoking@naver.com>
2020-11-21 21:56:12 +0900

    커밋 테스트

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,7 +10,7 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: Text('비만도 계산기'),
       ),
       body: Center(
         child: Text('Body Text'),
